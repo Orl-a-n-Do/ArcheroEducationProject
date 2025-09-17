5fe6bddfaa7eb44953ce0dd94ee6251de80b0086
Дмитрий <125256723+Orl-a-n-Do@users.noreply.github.com>
2025-09-17 20:19:59 +0300

    Package NewTon Json

--- Assets/_Project/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
@@ -1,7 +1,5 @@
 ﻿using Assets._Project.Develop.Runtime.Meta.Features.Wallet;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
 
 namespace Assets._Project.Develop.Runtime.Utilities.DataManagement
 {
