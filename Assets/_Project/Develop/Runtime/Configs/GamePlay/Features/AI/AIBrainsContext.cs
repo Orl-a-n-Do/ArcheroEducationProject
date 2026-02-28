2d277ae7018f20d06ed66fef4a5e7f058c12b35d
Orl-a-n-Do <luckypirat88@mail.ru>
2026-02-28 09:47:50 +0300

    AIBrainsContext
    AIBrainsContext

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/AI/AIBrainsContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.AI
+{
+    public  class AIBrainsContext
+    {
+        private readonly List<EntityToBrain> entityToBrains = new();
+        private class EntityToBrain
+        {
+            public Entity Entity;
+            public IBrain Brain;
+
+            public EntityToBrain(Entity entity, IBrain brain)
+            {
+                Entity = entity;
+                Brain = brain;
+            }
+        }
+
+
+    }
+}
