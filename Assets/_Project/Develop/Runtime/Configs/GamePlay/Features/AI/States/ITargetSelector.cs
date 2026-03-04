e17060d1250c5905fe794cda536cd87b3bbbc08e
Orl-a-n-Do <luckypirat88@mail.ru>
2026-03-04 11:25:56 +0300

    NearestDamageableTargetSelector

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/AI/States/ITargetSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.AI.States
+{
+    public interface ITargetSelector
+    {
+        Entity SelectTargetFrom(IEnumerable<Entity> targets);
+
+
+
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/AI/States/NearestDamageableTargetSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.AI.States
+{
+    public class NearestDamageableTargetSelector : ITargetSelector
+    {
+        private Entity _source;
+        private Transform _sourceTransform;
+
+        public NearestDamageableTargetSelector(Entity entity)
+        {
+            _source = entity;
+            _sourceTransform = entity.Transform;
+        }
+
+        public Entity SelectTargetFrom(IEnumerable<Entity> targets)
+        {
+            IEnumerable<Entity> selectTargets = targets.Where(target =>
+            {
+                return false;
+
+            });
+
+            return null;
+        }
+    }
+}
