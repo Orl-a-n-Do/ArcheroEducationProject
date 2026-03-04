63666119628c2eb4ddbe227c1bfcf2db888762a1
Orl-a-n-Do <luckypirat88@mail.ru>
2026-03-04 10:57:42 +0300

    Making a specific implementation
    Making a specific implementation

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/AI/AIParallelState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets._Project.Develop.Runtime.Utilities.StateMachineCore;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.AI
+{
+    public class AIParallelState: ParallelState<IUpdatableState>, IUpdatableState
+    {
+        public AIParallelState(params IUpdatableState[] states) : base(states)
+        {
+
+
+
+        }
+
+        public void Update(float deltaTime)
+        {
+            foreach(IUpdatableState state in States)
+                state.Update(deltaTime);
+        }
+    }
+}
