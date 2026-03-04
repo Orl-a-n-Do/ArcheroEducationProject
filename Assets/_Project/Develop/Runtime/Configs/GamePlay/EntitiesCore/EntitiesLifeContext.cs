6b638f027c2df0ac0cd8944067db83721332e973
Orl-a-n-Do <luckypirat88@mail.ru>
2026-03-04 12:47:38 +0300

    Current goal search status

--- Assets/_Project/Develop/Runtime/Configs/GamePlay/EntitiesCore/EntitiesLifeContext.cs
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/EntitiesCore/EntitiesLifeContext.cs
@@ -12,6 +12,8 @@ namespace Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore
         private readonly List<Entity> _entities = new();
         private readonly List<Entity> _releaseRequests = new();
 
+        public IReadOnlyList<Entity>Entities => _entities;
+
         public void Add(Entity entity)
         {
 
