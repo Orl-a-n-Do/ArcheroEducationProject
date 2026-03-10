dece96e5434b34cc87411baa631f8cbe5068b6a0
Orl-a-n-Do <luckypirat88@mail.ru>
2026-03-10 06:35:40 +0300

    Configs for Ghost and Hero

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Entities/GhostConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Entities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/NewGhostConfig", fileName = "GhostConfig")]
+    public class GhostConfig : EntityConfig
+    {
+        [field: SerializeField] public string PrefabPath { get; private set; } = "Entities/Ghost";
+        [field: SerializeField, Min(0)] public float FloatSpeed { get; private set; } = 9;
+        [field: SerializeField, Min(0)] public float RotationSpeed { get; private set; } = 900;
+        [field: SerializeField, Min(0)] public float MaxHealth { get; private set; } = 100;
+        [field: SerializeField, Min(0)] public float BodyContactDamage { get; private set; } = 50;
+        [field: SerializeField, Min(0)] public float DeathProcessTime { get; private set; } = 2;
+    }
+}
