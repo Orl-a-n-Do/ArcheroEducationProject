374256fb608109002afb425ae12214b13b5f72ff
Orl-a-n-Do <luckypirat88@mail.ru>
2026-03-31 23:35:34 +0300

    Stage change monitoring service

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/StagesFeatures/StageProviderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.Levels;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.StagesFeatures
+{
+    public class StageProviderService
+    {
+        private ReactiveVariable<int> _currentStageNumber = new();
+
+        private LevelConfig _levelConfig;
+        private StagesFactory _stagesFactory;
+
+        private IStage _currentStage;
+
+        public StageProviderService(
+            LevelConfig levelConfig, 
+            StagesFactory stagesFactory)
+        {
+            _levelConfig = levelConfig;
+            _stagesFactory = stagesFactory;
+        }
+    }
+}
