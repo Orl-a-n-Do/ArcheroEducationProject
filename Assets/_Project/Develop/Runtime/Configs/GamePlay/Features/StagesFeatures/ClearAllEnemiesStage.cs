fba354fa5f115eee5938a57576f9ffe0f9ab738d
Orl-a-n-Do <luckypirat88@mail.ru>
2026-03-31 22:34:39 +0300

    We are finishing the implementation of the enemy clearance mode

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/StagesFeatures/ClearAllEnemiesStage.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.Features.Enemies;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.Stages;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.StagesFeatures
+{
+    public class ClearAllEnemiesStage : IStage
+    {
+
+        private ClearAllEnemiesStageConfig _config;
+        private ReactiveEvent _completed = new();
+
+        private EnemiesFactory _enemiesFactory;
+        private EntitiesLifeContext _entitiesLifeContext;
+
+        private bool _inProcess;
+
+        private Dictionary<Entity, IDisposable> _spawnedEnemiesToRemoveReason = new();
+
+
+        public ClearAllEnemiesStage(
+            ClearAllEnemiesStageConfig config,
+            EnemiesFactory enemiesFactory,
+            EntitiesLifeContext entitiesLifeContext)
+        {
+            _config = config;
+            _enemiesFactory = enemiesFactory;
+            _entitiesLifeContext = entitiesLifeContext;
+        }
+
+        public IReadOnlyEvent Completed => _completed;
+
+        public void Cleanup()
+        {
+            foreach(KeyValuePair<Entity,IDisposable> item in _spawnedEnemiesToRemoveReason)
+            {
+                item.Value.Dispose();
+                _entitiesLifeContext.Release(item.Key);
+            }
+
+            _spawnedEnemiesToRemoveReason.Clear();
+
+            _inProcess = false;
+        }
+
+        public void Dispose()
+        {
+            foreach (KeyValuePair<Entity, IDisposable> item in _spawnedEnemiesToRemoveReason)
+            {
+                item.Value.Dispose();             
+            }
+
+           _spawnedEnemiesToRemoveReason.Clear();
+
+           _inProcess = false;
+        }
+
+        public void Start()
+        {
+            if (_inProcess)
+                throw new InvalidOperationException("Game modealready started");
+
+            SpawnEnemies();
+
+            _inProcess = true;
+        }
+
+
+        public void Update(float deltaTime)
+        {
+            if (_inProcess == false)
+                return;
+
+            if (_spawnedEnemiesToRemoveReason.Count == 0)
+                ProcessEnd();
+        }
+
+        private void ProcessEnd()
+        {
+            _inProcess = false;
+            _completed.Invoke();
+        }
+
+        private void SpawnEnemies()
+        {
+            foreach(EnemyItemConfig enemyItemConfig in _config.EnemyItems)
+                SpawnEnemy(enemyItemConfig);
+        }
+
+        private void SpawnEnemy(EnemyItemConfig enemyItemConfig)
+        {
+           Entity spawnedEnemy = _enemiesFactory.Create(enemyItemConfig.SpawnPosition, enemyItemConfig.EnemyConfig);
+
+            IDisposable removeReason = spawnedEnemy.IsDead.Subscribe((oldValue, isDead) =>
+            {
+                if (isDead)
+                {
+                    IDisposable disposable = _spawnedEnemiesToRemoveReason[spawnedEnemy];
+                    disposable.Dispose();
+                    _spawnedEnemiesToRemoveReason.Remove(spawnedEnemy);
+
+                }
+            });
+
+            _spawnedEnemiesToRemoveReason.Add(spawnedEnemy,removeReason);
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/StagesFeatures/IStage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.StagesFeatures
+{
+    public interface IStage : IDisposable
+    {
+        IReadOnlyEvent Completed { get; }
+
+        void Start();
+        void Update(float deltaTime);
+        void Cleanup();
+
+
+    }
+}
