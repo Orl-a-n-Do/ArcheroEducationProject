33aaea50f3f83bd0df1a1312bd85c2a011984a5d
Orl-a-n-Do <luckypirat88@mail.ru>
2026-04-02 13:23:44 +0300

    States of victory and defeat. Database

---
+++ Assets/_Project/Develop/Runtime/GamePlay/States/EndGameState.cs
@@ -0,0 +1,30 @@
+﻿using Assets._Project.Develop.Runtime.Configs.GamePlay.Features.InputFeature;
+using Assets._Project.Develop.Runtime.Utilities.StateMachineCore;
+
+namespace Assets._Project.Develop.Runtime.GamePlay.States
+{
+    public abstract class EndGameState: State
+    {
+        private readonly IInputService _inputService;
+
+        protected EndGameState(IInputService inputService)
+        {
+
+            _inputService = inputService;
+
+        }
+
+        public override void Enter()
+        {
+            base.Enter();   
+            _inputService.IsEnabled = false;
+        }
+
+        public override void Exit()
+        {
+            base.Exit();    
+            _inputService.IsEnabled = true;
+        }
+
+    }
+}
