e4bee2ed843308cfa122b9ea6706c1de603293db
Orl-a-n-Do <luckypirat88@mail.ru>
2026-04-03 11:09:02 +0300

    State of Defeat

---
+++ Assets/_Project/Develop/Runtime/GamePlay/States/DefeatState.cs
@@ -0,0 +1,46 @@
+﻿using ArcheroEducationProject.Assets._Project.Develop.Runtime.Utilities.SceneManagement;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.Features.InputFeature;
+using Assets._Project.Develop.Runtime.Utilities.CourutinesManagement;
+using Assets._Project.Develop.Runtime.Utilities.SceneManagement;
+using Assets._Project.Develop.Runtime.Utilities.StateMachineCore;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.GamePlay.States
+{
+    public class DefeatState : EndGameState, IUpdatableState
+    {
+      
+        private readonly SceneSwitcherService _sceneSwitcherService;
+        private readonly ICoroutinesPerformer _coroutinesPerformer;
+
+        public DefeatState(
+            IInputService inputService,
+            SceneSwitcherService sceneSwitcherService,
+            ICoroutinesPerformer coroutinesPerformer) : base(inputService)
+        {
+           
+            _sceneSwitcherService = sceneSwitcherService;
+            _coroutinesPerformer = coroutinesPerformer;
+        }
+
+        public override void Enter()
+        {
+
+            base.Enter();
+            Debug.Log("Поражение!");
+
+
+        }
+
+
+        public void Update(float deltaTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                _coroutinesPerformer.StartPerform(_sceneSwitcherService.ProcessSwitchTo(Scenes.MainMenu));
+
+            }
+
+        }
+    }
+}
