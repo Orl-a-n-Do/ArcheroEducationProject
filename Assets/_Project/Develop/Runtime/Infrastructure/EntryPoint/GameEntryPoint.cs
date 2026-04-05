996e705ac1edcf1e068574df592f68553948624a
Orl-a-n-Do <luckypirat88@mail.ru>
2026-04-05 11:48:36 +0300

    Putting together a gameplay cycle

--- Assets/_Project/Develop/Runtime/Infrastructure/EntryPoint/GameEntryPoint.cs
+++ Assets/_Project/Develop/Runtime/Infrastructure/EntryPoint/GameEntryPoint.cs
@@ -72,7 +72,7 @@ namespace Assets._Project.Develop.Runtime.Infrastructure.EntryPoint
 
             loadingScreen.Hide();
 
-            yield return sceneSwitcherService.ProcessSwitchTo(Scenes.Gameplay, new GameplayInputArgs(1));
+            yield return sceneSwitcherService.ProcessSwitchTo(Scenes.MainMenu);
 
         }
     
