fe591f0131f14f71daa41e46219ba026e174a6bc
Orl-a-n-Do <luckypirat88@mail.ru>
2026-05-11 12:54:57 +0300

    Making a WalkingView

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/MovementFeature/WalkingView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.GamePlay.Features.MovementFeature
+{
+    [RequireComponent(typeof(Animator))]
+    public class WalkingView: EntityView
+    {
+        private readonly int IsMovingKey = Animator.StringToHash("IsWalking");
+
+        [SerializeField] private Animator _animator;
+
+        private IReadOnlyVariable<bool> _isMoving;
+
+        private IDisposable _isMovingChangedDisposable;
+
+        public void OnValidate()
+        {
+            _animator ??= GetComponent<Animator>();
+        }
+
+        protected override void OnEntityStartedWork(Entity entity)
+        {
+            _isMoving = entity.IsMoving;
+
+            _isMovingChangedDisposable = _isMoving.Subscribe(OnIsMovingChanged);
+            UpdateIsMoving(_isMoving.Value);
+        }
+
+        public override void Cleanup(Entity entity)
+        {
+            base.Cleanup(entity);
+
+            _isMovingChangedDisposable.Dispose();
+
+        }
+
+        private void OnIsMovingChanged(bool oldIsMoving, bool isMoving) => UpdateIsMoving(isMoving);
+
+        private void UpdateIsMoving(bool value) => _animator.SetBool(IsMovingKey, value);
+
+    }
+}
