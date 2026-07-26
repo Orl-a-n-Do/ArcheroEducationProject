849cd69d9304cfb9670fe225eb0cdf28ec0052d4
Дмитрий <125256723+Orl-a-n-Do@users.noreply.github.com>
2026-07-26 13:24:09 +0300

    SomeWorkWithAnimations

--- Assets/_Project/Develop/Runtime/Configs/GamePlay/EntitiesCore/EntitiesFactory.cs
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/EntitiesCore/EntitiesFactory.cs
@@ -231,7 +231,7 @@ namespace Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore
 
             entity
                  .AddMoveDirection(new ReactiveVariable<Vector3>(direction))
-                 .AddMoveSpeed(new ReactiveVariable<float>(10))
+                 .AddMoveSpeed(new ReactiveVariable<float>(25))
                  .AddIsMoving()
                  .AddRotationDirection(new ReactiveVariable<Vector3>(direction))
                  .AddRotationSpeed(new ReactiveVariable<float>(9999))
