99c7dad662b65dfcd18a2fc9aa7825602982d291
Дмитрий <125256723+Orl-a-n-Do@users.noreply.github.com>
2026-09-07 20:55:15 +0300

    A script for changing the attack animation speed multiplier.

---
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/Features/Attack/InstantAttackAnimationSpeedView.cs
@@ -0,0 +1,32 @@
+﻿using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.Attack
+{
+    [RequireComponent(typeof(Animator))]
+    public class InstantAttackAnimationSpeedView : EntityView
+    {
+        private readonly int _attackAnimationSpeedMultiplierKey = Animator.StringToHash("AttackAnimationSpeedMultiplier");
+
+        [SerializeField] private AnimationClip _animationClip;
+        [SerializeField] private Animator _animator;
+
+        private ReactiveVariable<float> _attackProcessTime;
+
+        #region Сообщение Unity | Ссылок: 0
+        private void OnValidate()
+        {
+            _animator ??= GetComponent<Animator>();
+        }
+        #endregion
+
+        protected override void OnEntityStartedWork(Entity entity)
+        {
+            _attackProcessTime = entity.AttackProcessInitialTime;
+
+            _animator.SetFloat(_attackAnimationSpeedMultiplierKey, _animationClip.length / _attackProcessTime.Value);
+        }
+    }
+}
