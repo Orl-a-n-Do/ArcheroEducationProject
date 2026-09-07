3ab2d96f1aec2382f63438843a39699fa59973aa
Дмитрий <125256723+Orl-a-n-Do@users.noreply.github.com>
2026-09-07 22:55:21 +0300

    FixedBugWithDeadAnimations

--- AGENTS.md
+++ AGENTS.md
@@ -1,8 +1,13 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: ArcheroEducationProject
 - Unity version: Unity 2022.3.20f1
+- Active scene:
+  - Tags:
+    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController
+  - Layers:
+    - Default, TransparentFX, Ignore Raycast, Water, UI, Character, Projectiles, Enviroment
 - Active game object:
-  - Name: HeroView
+  - Name: Ghost
   - Tag: Untagged
-  - Layer: Default
+  - Layer: Character
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/_Project/Develop/Runtime/Configs/GamePlay/EntitiesCore/Mono/EntityView.cs
+++ Assets/_Project/Develop/Runtime/Configs/GamePlay/EntitiesCore/Mono/EntityView.cs
@@ -4,23 +4,22 @@ namespace Assets._Project.Develop.Runtime.Configs.GamePlay.EntitiesCore.Mono
 {
     public abstract class EntityView: MonoBehaviour
     {
-        public void Link(Entity entity) 
+        public void Link(Entity entity)
         {
-            entity.Initialized += OnEntityStartedWork;
-        
-        }
+            if (entity.IsInit)
+            {
+                OnEntityStartedWork(entity);
+                return;
+            }
 
-       
+            entity.Initialized += OnEntityStartedWork;
+        }
 
         public virtual void Cleanup(Entity entity)
         {
             entity.Initialized -= OnEntityStartedWork;
-
         }
 
         protected abstract void OnEntityStartedWork(Entity entity);
-        
-
-
     }
 }
